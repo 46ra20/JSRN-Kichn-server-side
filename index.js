2a1fd9d85bac215cdc362429154077e21aaf6648
46ra20 <rakib2046.md@gmail.com>
2022-11-07 11:11:09 -0500

    Add mongodb package

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 require('dotenv').config();
 
@@ -10,6 +11,12 @@ const app = express();
 // middle ware
 app.use(cors());
 app.use(express.json());
+
+//declare mongodb client
+
+const uri = process.env.URI;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
 
 
 //get response from express
